03f2a503c377ead4b5799b84560b694c5a331c43
ArslanRama <arslanrama84@gmail.com>
2021-06-07 16:44:11 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'facebookAuth': {
+    'clientID': '469793990785582', // your App ID
+    'clientSecret': '0a937e02dad925dd47ab279bb6c8c1a3', // your App Secret
+    'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+  }
+}
+
